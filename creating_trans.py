5eca16b40d07fb02268579b41408198c1a287390
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-12-07 21:12:47 -0500

    Initial Commit

--- creating_trans.py
+++ creating_trans.py
@@ -1,3 +1,11 @@
 from stellar_sdk import Asset, Keypair, Network, Server, TransactionBuilder
 from stellar_sdk.exceptions import NotFoundError, BadResponseError, BadRequestError
 
+
+class Contract:
+    
+    server = server = Server("https://horizon-testnet.stellar.org")
+
+    def __init__(self, server=self.server):
+        target_source_key = str(input("Please enter the name of the user you would like to send a transactiont to: "))
+        

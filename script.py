a0b11f975505c3f124024d5ae9435828f602e499
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-12-08 14:32:24 -0500

    Interacting Commands

--- script.py
+++ script.py
@@ -12,10 +12,23 @@ class Script:
         time.sleep(0.2)
         if testnet_acc.signed_in != True:
             print(colored("You have not signed in yet", 'red'))
-            print(testnet_acc)
-        else:
-            print(colored("Welcome to Stellar", 'blue'))
-            time.sleep(0.2)
-            print("/create_trans: ")
+            while testnet_acc.signed_in != True:
+                print(testnet_acc)
+        
+        print(colored("Welcome to Stellar", 'blue'))
+        time.sleep(0.2)
+        print("/create_trans: to create a transaction")
+        print("-"*24)
+        time.sleep(0.2)
+        print("/receive_trans: to check or receive transactions")
+        print("-"*24)
+        time.sleep(0.2)
 
-            
+        
+        
+        
+
+
+test = Script()
+
+print(test)

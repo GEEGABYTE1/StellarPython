73c7839c7ac667548a717562a3e5800d16ab10d3
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-12-08 14:29:47 -0500

    Combining Classes

--- script.py
+++ script.py
@@ -1,7 +1,21 @@
 from account import testnet_acc # Creating Account
+from creating_trans import Contract # Creating/Sending Transaction
+
+import time
+from termcolor import colored
 
 
 class Script:
 
     def __init__(self):
-        pass
+        print("-"*24)
+        time.sleep(0.2)
+        if testnet_acc.signed_in != True:
+            print(colored("You have not signed in yet", 'red'))
+            print(testnet_acc)
+        else:
+            print(colored("Welcome to Stellar", 'blue'))
+            time.sleep(0.2)
+            print("/create_trans: ")
+
+            

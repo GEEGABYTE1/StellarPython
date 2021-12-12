1af94b06ffa5790e2fb68c4b4f0301c8db95332b
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-12-12 18:47:02 -0500

    Sign in/out quit

--- account.py
+++ account.py
@@ -30,44 +30,55 @@ class Account:
         
 
     def sign_up(self):
-        time.sleep(0.2)
-        print("\n")
-        print(colored("If you don't have a public key, please type /create or /fetch_pk to fetch your public key", 'blue'))
-        public_key = str(input("Please print your public key: "))
-        username = str(input("Please type in a username: "))
-        public_key = public_key.strip(" ")
-        public_key = public_key.lower()
-        if public_key == '/create':
-            public_key = self.create_account()
-            acc = {'Username': username, 'PKey': public_key, 'Transactions': [], 'Paging_Token': None, 'Skey': self.keys[0]}
-            db.insert_one(acc)
-        elif public_key == '/fetch_pk':
-            if len(self.keys) == 0:
-                print("There is no key created or saved at this time")
-                time.sleep(0.2)
-            else:
-                public_key = self.keys[-1]
-            
-        
-        response = requests.get(f"https://friendbot.stellar.org?addr={public_key}")
-        if response.status_code == 200:
-            print(colored("Success, you have successfully created an account! ", 'green'))
+        try:
             time.sleep(0.2)
-            print('You can now sign in! ')
-        else:
+            print("\n")
+            print(colored("If you don't have a public key, please type /create or /fetch_pk to fetch your public key", 'blue'))
+            public_key = str(input("Please print your public key: "))
+            if public_key == '/quit':
+                return 
+            username = str(input("Please type in a username: "))
+            if username == '/quit':
+                return
+            public_key = public_key.strip(" ")
+            public_key = public_key.lower()
+            if public_key == '/create':
+                public_key = self.create_account()
+                acc = {'Username': username, 'Pkey': public_key, 'Transactions': [], 'Paging_Token': None, 'Skey': self.keys[0]}
+                db.insert_one(acc)
+            elif public_key == '/fetch_pk':
+                if len(self.keys) == 0:
+                    print("There is no key created or saved at this time")
+                    time.sleep(0.2)
+                else:
+                    public_key = self.keys[-1]
+
+        
+            response = requests.get(f"https://friendbot.stellar.org?addr={public_key}")
+            if response.status_code == 200:
+                print(colored("Success, you have successfully created an account! ", 'green'))
+                time.sleep(0.2)
+                print('You can now sign in! ')
+            else:
+                print(colored("Oops, there seems to be an error creating your account ", 'red'))
+
+        except:
             print(colored("Oops, there seems to be an error creating your account ", 'red'))
 
     def sign_in(self, public_key):
-        server = Server("https://horizon-testnet.stellar.org")
-        account = server.accounts().account_id(public_key).call()
         try:
-            for balance in account['balances']:
-                print("Type: {}".format(balance['asset_type']))
-                time.sleep(0.1)
-                print("Balance: {}".format(balance['balance']))
+            server = Server("https://horizon-testnet.stellar.org")
+            account = server.accounts().account_id(public_key).call()
+            try:
+                for balance in account['balances']:
+                    print("Type: {}".format(balance['asset_type']))
+                    time.sleep(0.1)
+                    print("Balance: {}".format(balance['balance']))
+            except:
+                print(colored("That public key seems to be invalid", 'red'))
+                return False
         except:
-            print(colored("That public key seems to be invalid", 'red'))
-            return False
+            print(colored("Oops, something went wrong", 'red'))
     
     def __init__(self):
         self.signed_in = False
@@ -88,6 +99,8 @@ class Account:
             elif prompt == '/sign_in':
                 print(colored("You can use /fetch_pk to fetch your public key if needed! ", 'blue'))
                 user_key = str(input("Please type in your public key: "))
+                if user_key == '/quit':
+                    continue 
                 if user_key == '/fetch_pk':
                     print("Public Key: {}".format(self.keys[-1]))
                     user_key = self.keys[-1]

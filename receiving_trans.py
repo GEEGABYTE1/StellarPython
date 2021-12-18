fdba0162bba9d6cc7a674df5d7b17fb72718c29f
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-12-17 20:08:28 -0500

    Logging Transactions

--- receiving_trans.py
+++ receiving_trans.py
@@ -16,12 +16,13 @@ def load_last_paging_token():
 def save_paging_token(paging_token):
     pass
     
+server = testnet_acc.server
 
 def fetch_users(fetched=False):
-    server = testnet_acc.server
+    
     account_server = db.find({})
     try:
-        user = str(input("Please enter the name of the sender you would like to see transactions from: "))
+        user = str(input("Please enter the sender you want track transitions from: "))
         print("Finding user...")
         time.sleep(0.2)
         
@@ -37,36 +38,36 @@ def fetch_users(fetched=False):
         print(colored("User not found", 'red'))
 
 def receieve_payment():
-    try:
-        while True:
-            fetched_user = fetch_users()
-            if type(fetched_user) != tuple:
-                continue
-            else:
-                break
-        
-            
-        user_id = fetched_user[0]
-        payments = server.payments().for_account(user_id)
-        
-        last_token = load_last_paging_token()
-        if last_token:
-            payments.cursor(last_token)
-            
-            for payment in payments.stream():
-                save_paging_token(payment['paging_token'])
-
-                if payment['type'] != 'payment':
-                    continue 
-                if payment['to'] != user_id:
+        try:
+            while True:
+                fetched_user = fetch_users()
+                if type(fetched_user) != tuple:
                     continue
-                if payment['asset_type'] == 'native':
-                    asset = 'Lumens'
                 else:
-                    asset = f"{payment['asset_code']}:{payment['asset_issuer']}"
-                print("Payment: {amount} from payment from {sender}".format(amount=asset, sender=payment['from']))
-    except:
-        print(colored('The account {} could not be found '.format(user_id), 'red'))
+                    break
+            
+                
+            user_id = fetched_user[0]
+            payments = server.payments().for_account(user_id)
+            
+            last_token = load_last_paging_token()
+            if last_token:
+                payments.cursor(last_token)
+                
+                for payment in payments.stream():
+                    save_paging_token(payment['paging_token'])
+
+                    if payment['type'] != 'payment':
+                        continue 
+                    if payment['to'] != user_id:
+                        continue
+                    if payment['asset_type'] == 'native':
+                        asset = 'Lumens'
+                    else:
+                        asset = f"{payment['asset_code']}:{payment['asset_issuer']}"
+                    print("Payment: {amount} from payment from {sender}".format(amount=asset, sender=payment['from']))
+        except:
+            print(colored('The account could not be found ', 'red'))
 
 
 
